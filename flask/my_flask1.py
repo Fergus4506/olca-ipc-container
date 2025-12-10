d85f82a7fe2b2dc41f2d1c6409f6132c7f8215c1
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2025-12-10 22:20:36 +0800

    [fix] 模型參數變數對應錯誤修正

--- flask/my_flask1.py
+++ flask/my_flask1.py
@@ -71,8 +71,8 @@ def calculate_openlca(distance, factor, load, amount):
         amount=amount,
         impact_method=method,
         parameters=[
-            o.ParameterRedef(name=parameters[0].name, value=distance, context=parameters[0].context),
-            o.ParameterRedef(name=parameters[1].name, value=factor, context=parameters[1].context),
+            o.ParameterRedef(name=parameters[0].name, value=factor, context=parameters[0].context),
+            o.ParameterRedef(name=parameters[1].name, value=distance, context=parameters[1].context),
             o.ParameterRedef(name=parameters[2].name, value=load, context=parameters[2].context),
         ],
     )
